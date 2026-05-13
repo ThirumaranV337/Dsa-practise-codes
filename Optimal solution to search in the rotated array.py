d121f08fb0bf4be6eddfe21ff5ddb0bdb2b9389d
Thirumaran V <thirumarant337@gmail.com>
2026-05-13 10:11:00 +0530

    Implement binary search in rotated sorted array

---
+++ Optimal solution to search in the rotated array.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def search(self, nums, target):
+        left=0
+        right=len(nums)-1
+        while left<=right:
+            mid=(left+right)//2
+            if nums[mid]==target:
+                return mid
+            elif nums[left]<=nums[mid]:## we checking the right half is first sorted or not 
+                if nums[left]<=target and nums[mid]>=target:## if the right half is sorted then we finding the target is falling the range or not 
+                    right=mid-1#if it true skip the left side 
+                else:
+                    left=mid+1#if it is false skip the right side 
+            else:##then came to here if the left is not sorted or the value are not in the range 
+                if nums[mid]<=target and nums[right]>=target:## checking the target is falling inside the range or not 
+                    left=mid+1##skipping the left side part 
+                else:
+                    right=mid-1##skipping the right side part
+        return -1
