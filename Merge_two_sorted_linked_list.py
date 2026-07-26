31640f6a047b9c26045754947f16d7a5cff31b12
Thirumaran V <thirumarant337@gmail.com>
2026-07-26 12:02:52 +0530

    Create Merge_two_sorted_linked_list.py

---
+++ Merge_two_sorted_linked_list.py
@@ -0,0 +1,31 @@
+
+class Node:
+    def __init__(self, data): 
+        self.data = data
+        self.next = None
+
+
+class Solution:
+    def sortedMerge(self, head1, head2):
+        curr_1=head1
+        curr_2=head2
+        dummy_node=Node(-1)
+        temp=dummy_node
+        while curr_1 and curr_2:
+            if curr_1.data>=curr_2.data:
+                temp.next=curr_2
+                temp=temp.next
+                curr_2=curr_2.next
+            elif curr_2.data>=curr_1.data:
+                temp.next=curr_1
+                temp=temp.next
+                curr_1=curr_1.next
+        if not curr_1:
+            temp.next=curr_2
+        else:
+            temp.next=curr_1
+        return dummy_node.next
+            
+                
+        
+        
